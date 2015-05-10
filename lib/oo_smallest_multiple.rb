521151026ab7697cc8d56a77522ae0b59d241a5c
axton21 <axton_n@yahoo.com>
2015-05-10 10:31:53 -0400

    done

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,23 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+	attr_reader :range
+
+	def initialize(range)
+		@range = range
+	end
+
+	def lcm
+		number = 1
+		boolean = false
+		until boolean == true do
+			for i in 2..range
+				if number % i  != 0 then
+					break
+				end
+				if i == range && number % i == 0
+					return number
+				end
+			end
+			number += 1
+		end
+	end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def smallest_multiple(num_range) #1..5 60
+	number = 1
+	boolean = false
+	until boolean == true do
+		for i in 2..num_range
+			if number % i  != 0 then
+				break
+			end
+			if i == num_range && number % i == 0
+				return number
+			end
+		end
+		number += 1
+	end
+end
